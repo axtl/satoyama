8907e57de06f9ed944dcbd9899468c348aeb34fc
alexandru <alex@hackd.net>
2011-03-30 12:29:05 -0700

    web.py barebones server

---
+++ warusawa/server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""A web.py application powered by gevent"""
+# https://bitbucket.org/denis/gevent/src/960ab8d1352f/examples/webpy.py
+
+from gevent import monkey
+monkey.patch_all()
+from gevent.wsgi import WSGIServer
+import gevent
+import sys
+import web
+
+urls = ("/", "index")
+
+
+class index:
+
+    def GET(self):
+        return 'Hello, world!'
+
+
+if __name__ == "__main__":
+    application = web.application(urls, globals()).wsgifunc()
+    try:
+        sys.stdout.write('Serving on 8088...\n')
+        sys.stdout.flush()
+        WSGIServer(('', 8088), application).serve_forever()
+    except KeyboardInterrupt:
+        sys.stdout.write("Exiting...\n")
