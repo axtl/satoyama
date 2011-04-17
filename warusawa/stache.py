bc467cf8afb0d508dd1ef4c6f45344b807f295d2
alexandru <alex@hackd.net>
2011-04-17 16:39:50 -0700

    remove old testing code

--- warusawa/stache.py
+++ warusawa/stache.py
@@ -4,25 +4,6 @@ import pystache
 TMPL = 'templates'
 
 
-# TESTING #
-c1 = "this is a longer comment because we need to test some crap, and\
-     lorem ipsum is such a bother, really, that I'm not even going to \
-     try and pretend..."
-c2 = "Quisque lacus quam, egestas ac tincidunt a, lacinia vel velit. \
-     Aenean facilisis nulla vitae urna tincidunt congue sed ut dui. \
-     Morbi malesuada nulla nec purus convallis consequat. Vivamus id \
-     mollis quam. Morbi ac commodo nulla. In condimentum orci id nisl \
-     volutpat bibendum. Quisque commodo hendrerit lorem quis egestas. \
-     Maecenas quis tortor arcu. Vivamus rutrum nunc non neque \
-     consectetur quis placerat neque lobortis. Nam vestibulum, arcu \
-     sodales feugiat consectetur, nisl orci bibendum elit, eu euismod \
-     magna sapien ut nibh. Donec semper quam scelerisque tortor \
-     dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, \
-     odio sed rhoncus suscipit, sem diam ultrices mauris, eu \
-     consequat purus metus eu velit. Proin metus."
-
-# NO MORE TESTING #
-
 def _trim_post(post_text):
     if len(post_text) > THR:
         short = post_text[:(THR - 5)] + ' ...'
