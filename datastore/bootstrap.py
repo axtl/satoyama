a7887d08f55a8eb474b82f0fae7cd8b212475054
alexandru <alex@hackd.net>
2011-04-16 21:13:56 -0700

    comment list and counter added to bootstrap code; better comments

--- datastore/bootstrap.py
+++ datastore/bootstrap.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+from datetime import datetime
 import redis
 import sys
 
@@ -12,18 +12,33 @@ def main(argv):
         max_pid = 10
     r = redis.Redis(host='localhost', port=6379, db=0)
     r.flushall()
+    # global post counter
+    npid = 0
 
     def post(post_id, frag):
-        return 'posts:%s:%s' % (npid, frag)
+        return 'posts:%s:%s' % (post_id, frag)
 
-    for pid in xrange(max_pid):
+    def comm(post_id, comm_id):
+        return 'posts:%s:comm:%s' % (post_id, comm_id)
+
+    while npid < max_pid:
+        # increment the global post id counter
         npid = r.incr('next.post.id')
+        # store in the posts list
         r.lpush('post.list', npid)
+        # store in the id field of the post itself, for simplicity
         r[post(npid, 'post_id')] = npid
+        # post title
         r[post(npid, 'title')] = 'TITLE: %s' % npid
+        # post body
         r[post(npid, 'body')] = 'This is post %s' % npid
-        r.rpush(post(npid, 'comms'), 'A comment for post %s' % npid)
-        r.rpush(post(npid, 'comms'), 'TROLLFACE >:@')
+        # increment per-post comment counter
+        ncid = r.incr(post(npid, 'next.comm.id'))
+        # store in the per-post list of active comments
+        r.lpush(post(npid, 'comm.list'), ncid)
+        # and store the post
+        r[comm(npid, ncid)] = 'A comment for post %s' % npid
+        r[comm(npid, ncid) + ':date'] = datetime.utcnow()
 
 if __name__ == '__main__':
     main(sys.argv)
